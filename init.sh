84193a55fd4f2477c2ef56aa369d44fd730b2083
Mikhail Demashkevich <demashkevich@gmail.com>
2018-12-23 15:41:33 +0300

    Comment "Bakytzhan" v2

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
- sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
- sudo rm -rf /etc/nginx/sites-enabled/default
- sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo rm -rf /etc/nginx/sites-enabled/default
+
+sudo /etc/init.d/gunicorn restart
+
+sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
+sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
