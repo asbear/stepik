8b0e25889e850e1cc2fd2c3b37ea5b0284cc449c
Mikhail Demashkevich <demashkevich@gmail.com>
2019-01-16 23:00:28 +0300

    step 2.1 start gunicorn without conf

--- init.sh
+++ init.sh
@@ -5,5 +5,8 @@ sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 
 sudo /etc/init.d/gunicorn restart
 
-sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
-sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
+# sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
+# sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
+
+gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &     
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application & 
